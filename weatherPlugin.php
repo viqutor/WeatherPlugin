913d5a227d8a74e627bb82dae48fd584ced760d2
Victor Fulcher <vrfulcher@gmail.com>
2022-09-16 17:27:27 -0600

    Shortcode Stylized

--- weatherPlugin.php
+++ weatherPlugin.php
@@ -6,14 +6,23 @@
 
  */
 
-function weatherShortcode($htmlContent){
+function weatherShortcode(){
     $url = file_get_contents('https://api.openweathermap.org/data/2.5/weather?lat=47.5053&lon=-111.3008&appid=24fcd672aebb7d5f43302c4059e48e19&units=imperial');
 
     $weatherData = json_decode($url, true); //Change JSON to PHP Variables
     
     if(!empty($weatherData)){ //check if array is not empty
 
-		$output = '<div class="container"><h2 id="weatherHeader">Current Temp for Great Falls, MT:</h2><h2 id="weatherTemp">' . $weatherData['main']['temp'] . '</h2></div>';
+		$output = '<div class="container">
+					<div class="locationHeader">
+						<h1 id="heading"> ' . $weatherData['name'] . '</h1>
+						<p>Current Weather: ' . $weatherData['weather'][0]['description'] . ', ' . (int)$weatherData['main']['temp'] . ' degrees.</p>
+					</div>
+					<div class="weatherContainer">
+						<p>Today has a max temperature of: ' . (int)$weatherData['main']['temp_max'] . '.</p>
+						<p>Today has a minimum temperature of: ' . (int)$weatherData['main']['temp_min'] . '.</p>
+					</div>
+					</div>';
 
 		return $output;
         
