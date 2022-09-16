c1c90741856e3addf861f47e07118e3a69a3dd42
Victor Fulcher <45213824+viqutor@users.noreply.github.com>
2022-09-16 15:50:15 -0600

    Shortcode displaying API

--- weatherPlugin.php
+++ weatherPlugin.php
@@ -1,24 +1,40 @@
 <?php
 /*
- Plugin Name: Custom Weather Plugin
- Author: Victor Fulcher
- Description: A custom weather plugin for a live coding interview! :)
+ *Plugin Name: Custom Weather Plugin
+ *Author: Victor Fulcher
+ *Description: A custom weather plugin for a live coding interview! :)
 
  */
 
-$url = 'https://api.openweathermap.org/data/2.5/weather?lat=47.5053&lon=-111.3008&appid=24fcd672aebb7d5f43302c4059e48e19&units=imperial';
+function weatherShortcode($htmlContent){
+    $url = file_get_contents('https://api.openweathermap.org/data/2.5/weather?lat=47.5053&lon=-111.3008&appid=24fcd672aebb7d5f43302c4059e48e19&units=imperial');
 
-function weatherShortcode(){
-    $weatherData = json_decode(file_get_contents($url), true); //Change JSON to PHP Variables
+    $weatherData = json_decode($url, true); //Change JSON to PHP Variables
     
-    echo "hello world";
-    print_r($url, true);
-    echo $weatherData['temp'];
-    //return $weatherData->{'main'};
-    return print_r($weatherData);
+    if(!empty($weatherData)){ //check if array is not empty
 
+		$output = '<div class="container"><h2 id="weatherHeader">Current Temp for Great Falls, MT:</h2><h2 id="weatherTemp">' . $weatherData['main']['temp'] . '</h2></div>';
+
+		return $output;
+        
+    } else { //if array is empty, say so
+        echo 'empty';
     }
-
+    
+    }
 add_shortcode('getWeather', 'weatherShortcode');
 
+/** Enqueuing the Stylesheet for the Weather Box */
+
+function enqueue_scripts() {
+	global $post;
+	if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'getWeather') ) {
+	wp_register_style( 'weatherStylesheet',  plugin_dir_url( __FILE__ ) . 'style.css' );
+		wp_enqueue_style( 'weatherStylesheet' );
+	}
+   }
+   add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
+
+
 ?>
+
